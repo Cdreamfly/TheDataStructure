1a371820faaceb646162b1a2d860eb08bec397fd
Cdreamfly <2290660986@qq.com>
2022-07-06 17:32:57 +0800

    线段树完结

--- include/SegmentTree.h
+++ include/SegmentTree.h
@@ -11,8 +11,10 @@ class SegmentTree {
 public:
     SegmentTree() noexcept = default;
 
-    explicit SegmentTree(T *arr, int n, std::function<T(T, T)> func) : data(new T[n]), tree(new T[4 * n]),
-                                                                       size(n), function(func) {
+    explicit SegmentTree(const T *const arr, const int n, std::function<T(T, T)> func) : data(new T[n]),
+                                                                                         tree(new T[4 * n]),
+                                                                                         size(n),
+                                                                                         function(func) {
         for (int i = 0; i < n; ++i) {
             data[i] = arr[i];
         }
@@ -21,7 +23,10 @@ public:
     }
 
     ~SegmentTree() noexcept {
-
+        delete[] data;
+        data = nullptr;
+        delete[] tree;
+        tree = nullptr;
     }
 
     constexpr int getSize() const noexcept {
@@ -33,7 +38,18 @@ public:
         return data[index];
     }
 
-    void print() {
+    T query(const int queryL, const int queryR) {
+        assert(queryL >= 0 && queryL < size && queryR >= 0 && queryR < size && queryL <= queryR);
+        return query(0, 0, size - 1, queryL, queryR);
+    }
+
+    void set(const int index, const T &e) {
+        assert(index >= 0 && index < size);
+        data[index] = e;
+        set(0, 0, size - 1, index, e);
+    }
+
+    void print() const {
         std::cout << "[";
         for (int i = 0; i < size * 4; ++i) {
             if (tree[i] != NULL) {
@@ -49,28 +65,72 @@ public:
     }
 
 private:
-    void buildSegmentTree(int treeIndex, int left, int right) {
-        //如果左右相等就说明递归到底，
+
+    void set(const int treeIndex, const int l, const int r, const int index, const T &e) {
+        //都叶子了，一定是它了，更新它
+        if (l == r) {
+            tree[treeIndex] = e;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        //要找的索引大于中间值，一定在右边
+        if (index >= mid + 1) {
+            set(rightTreeIndex, mid + 1, r, index, e);
+        } else if (index <= mid) {  //否则在左边
+            set(leftTreeIndex, l, mid, index, e);
+        }
+        //更新...
+        tree[treeIndex] = function(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    //在以treeIndex为根的线段树[l...r]的范围里，搜索区间[queryL,queryR]的值
+    int query(const int treeIndex, const int l, const int r, const int queryL, const int queryR) {
+        //如果左右相同就找到了
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        //如果查找的范围左边界大于中间
+        if (mid + 1 <= queryL) {
+            //那么就不用查找左边
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+            //如果查找的范围右边小于中间
+        } else if (mid >= queryR) {
+            //那么就不用查找右边
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }
+        //如果查找的范围占用两个区间
+        T leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        T rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        return function(leftResult, rightResult);
+    }
+
+    void buildSegmentTree(const int treeIndex, const int left, const int right) {
+        //如果左右相等就说明递归到底
         if (left == right) {
             tree[treeIndex] = data[left];
             return;
         }
-        int leftChildIndex = leftChild(treeIndex);
-        int rightChildIndex = rightChild(treeIndex);
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
         int mid = left + (right - left) / 2;
-        //递归左右孩子
-        buildSegmentTree(leftChildIndex, left, mid);
-        buildSegmentTree(rightChildIndex, mid + 1, right);
+        //递归左右孩子根为左右孩子索引，左右边界以中间为界
+        buildSegmentTree(leftTreeIndex, left, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, right);
         //线段存储信息根据业务写相应的代码，以求和为例，
-        tree[treeIndex] = function(tree[leftChildIndex], tree[rightChildIndex]);
+        tree[treeIndex] = function(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
     constexpr int leftChild(const int index) const noexcept {
-        return index * 2;
+        return index * 2 + 1;
     }
 
     constexpr int rightChild(const int index) const noexcept {
-        return index * 2 + 1;
+        return index * 2 + 2;
     }
 
 private:
--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ int main() {
     SegmentTree<int> *segmentTree = new SegmentTree<int>(nums, sizeof(nums) / sizeof(int), [](int a, int b) -> int {
         return a + b;
     });
+    std::cout << segmentTree->query(0,2) << std::endl;
+    std::cout << segmentTree->query(2,5) << std::endl;
+    std::cout << segmentTree->query(0,5) << std::endl;
     segmentTree->print();
+    segmentTree->set(0,0);
+    segmentTree->print();
+    std::cout << segmentTree->query(0,2) << std::endl;
+    std::cout << segmentTree->query(2,5) << std::endl;
+    std::cout << segmentTree->query(0,5) << std::endl;
+
     return 0;
 }
