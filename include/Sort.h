d08410f7cdf3a81a9fe29cc8e2b12c5d495f536d
Cdreamfly <2290660986@qq.com>
2022-07-16 22:52:05 +0800

    快速排序

--- include/Sort.h
+++ include/Sort.h
@@ -213,4 +213,111 @@ namespace Sort {
             }
         }
     }
+
+    // 对arr[l...r]部分进行partition操作
+    // 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+    template<typename T>
+    int __partition1(T arr[], const int l, const int r) {
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        std::swap(arr[l], arr[rand() % (r - l + 1) + l]);
+        const T v = arr[l];
+        int j = l;
+        // arr[l+1...j] < v ; arr[j+1...i) > v
+        for (int i = l + 1; i <= r; i++) {
+            if (arr[i] < v) {
+                std::swap(arr[++j], arr[i]);
+            }
+        }
+        std::swap(arr[l], arr[j]);
+        return j;
+    }
+
+    // 双路快速排序的partition
+    // 返回p, 使得arr[l...p-1] <= arr[p] ; arr[p+1...r] >= arr[p]
+    // 双路快排处理的元素正好等于arr[p]的时候要注意，详见下面的注释：）
+    template<typename T>
+    int __partition2(T arr[], int l, int r) {
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        std::swap(arr[l], arr[rand() % (r - l + 1) + l]);
+        const T v = arr[l];
+        // arr[l+1...i) <= v; arr(j...r] >= v
+        int i = l + 1, j = r;
+        while (true) {
+            // 注意这里的边界, arr[i] < v, 不能是arr[i] <= v
+            // 思考一下为什么?
+            while (i <= r && arr[i] < v)++i;
+            // 注意这里的边界, arr[j] > v, 不能是arr[j] >= v
+            // 思考一下为什么?
+            while (j >= l + 1 && arr[j] > v)--j;
+            if (i > j)break;
+            std::swap(arr[i], arr[j]);
+            //arr[i] < v. 在碰到很多连续相同数字的情况下，i只向后移动一次，同时j至少向前移动一次，相对平衡。
+            //arr[i] <= vb, 在碰到很多连续相同数字的情况下, i首先不停向后移动，直到满足条件为止，造成不平衡。
+            ++i;
+            --j;
+        }
+        std::swap(arr[l], arr[j]);
+        return j;
+    }
+
+    // 对arr[l...r]部分进行快速排序
+    template<typename T>
+    void __quickSort(T arr[], const int l, const int r) {
+        // 对于小规模数组, 使用插入排序进行优化
+        if (r - l <= 15) {
+            insertionSort(arr, l, r);
+            return;
+        }
+        const int p = __partition2(arr, l, r);
+        __quickSort(arr, l, p - 1);
+        __quickSort(arr, p + 1, r);
+    }
+
+    //快速排序
+    template<typename T>
+    void quickSort(T arr[], const int n) {
+        srand(time(NULL));
+        __quickSort(arr, 0, n - 1);
+    }
+
+    // 递归的三路快速排序算法
+    template<typename T>
+    void __quickSort3Ways(T arr[], const int l, const int r) {
+
+        // 对于小规模数组, 使用插入排序进行优化
+        if (r - l <= 15) {
+            insertionSort(arr, l, r);
+            return;
+        }
+
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        std::swap(arr[l], arr[rand() % (r - l + 1) + l]);
+
+        T v = arr[l];
+
+        int lt = l;     // arr[l+1...lt] < v
+        int gt = r + 1; // arr[gt...r] > v
+        int i = l + 1;    // arr[lt+1...i) == v
+        while (i < gt) {
+            if (arr[i] < v) {
+                std::swap(arr[i], arr[lt + 1]);
+                i++;
+                lt++;
+            } else if (arr[i] > v) {
+                std::swap(arr[i], arr[gt - 1]);
+                gt--;
+            } else { // arr[i] == v
+                i++;
+            }
+        }
+        std::swap(arr[l], arr[lt]);
+        __quickSort3Ways(arr, l, lt - 1);
+        __quickSort3Ways(arr, gt, r);
+    }
+    // 对于包含有大量重复数据的数组, 三路快排有巨大的优势
+    template<typename T>
+    void quickSort3Ways(T arr[], const int n) {
+        srand(time(nullptr));
+        __quickSort3Ways(arr, 0, n - 1);
+    }
 };
--- main.cpp
+++ main.cpp
@@ -2,42 +2,63 @@
 #include "SortTestHelper.h"
 
 using namespace std;
-
+// 比较Merge Sort和双路快速排序和三路快排三种排序算法的性能效率
+// 对于包含有大量重复数据的数组, 三路快排有巨大的优势
+// 对于一般性的随机数组和近乎有序的数组, 三路快排的效率虽然不是最优的, 但是是在非常可以接受的范围里
+// 因此, 在一些语言中, 三路快排是默认的语言库函数中使用的排序算法。比如Java:)
 int main() {
-    int n = 2000000;
 
-    cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
-    int *arr1 = SortTestHelper::generateRandomArray(n, 0,n);
-    int *arr2 = SortTestHelper::copyIntArray(arr1, n);
-    int *arr3 = SortTestHelper::copyIntArray(arr1, n);
-    int *arr4 = SortTestHelper::copyIntArray(arr1, n);
-    int *arr5 = SortTestHelper::copyIntArray(arr1, n);
+    int n = 1000000;
 
-//    SortTestHelper::printArray(arr1, n);
-//    SortTestHelper::printArray(arr2, n);
-//    SortTestHelper::printArray(arr3, n);
-//    SortTestHelper::printArray(arr4, n);
-//    SortTestHelper::printArray(arr5, n);
+    // 测试1 一般性测试
+    cout<<"Test for random array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
+    int* arr1 = SortTestHelper::generateRandomArray(n,0,n);
+    int* arr2 = SortTestHelper::copyIntArray(arr1,n);
+    int* arr3 = SortTestHelper::copyIntArray(arr1,n);
 
-
-    //SortTestHelper::testSort("Insertion Sort", Sort::insertionSort, arr1, n);
-    //SortTestHelper::testSort("Selection Sort", Sort::selectionSort, arr2, n);
-    //SortTestHelper::testSort("bubbleSort Sort", Sort::bubbleSort, arr3, n);
-    SortTestHelper::testSort("shellSort Sort", Sort::shellSort, arr4, n);
-    SortTestHelper::testSort("mergeSort Sort", Sort::mergeSort, arr5, n);
-
-//    SortTestHelper::printArray(arr1, n);
-//    SortTestHelper::printArray(arr2, n);
-//    SortTestHelper::printArray(arr3, n);
-//    SortTestHelper::printArray(arr4, n);
-//    SortTestHelper::printArray(arr5, n);
-
+    SortTestHelper::testSort("Merge Sort", Sort::mergeSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort", Sort::quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", Sort::quickSort3Ways, arr3, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
-    delete[] arr4;
-    delete[] arr4;
 
-    cout << endl;
+    cout<<endl;
+
+
+    // 测试2 测试近乎有序的数组
+    int swapTimes = 100;
+    cout<<"Test for nearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
+    arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
+    arr2 = SortTestHelper::copyIntArray(arr1, n);
+    arr3 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Merge Sort", Sort::mergeSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort", Sort::quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", Sort::quickSort3Ways, arr3, n);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+
+    cout<<endl;
+
+
+    // 测试3 测试存在包含大量相同元素的数组
+    cout<<"Test for random array, size = "<<n<<", random range [0,10]"<<endl;
+    arr1 = SortTestHelper::generateRandomArray(n,0,10);
+    arr2 = SortTestHelper::copyIntArray(arr1, n);
+    arr3 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Merge Sort", Sort::mergeSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort", Sort::quickSort, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3 Ways", Sort::quickSort3Ways, arr3, n);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+
+
+    return 0;
 }
