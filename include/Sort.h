7de7b5271222437e0dbaee7ae5591fad744a4b18
Cdreamfly <2290660986@qq.com>
2022-07-16 15:50:13 +0800

    归并排序

--- include/Sort.h
+++ include/Sort.h
@@ -67,6 +67,19 @@ namespace Sort {
             arr[j] = e;
         }
     }
+
+    // 对arr[l...r]范围的数组进行插入排序
+    template<typename T>
+    void insertionSort(T arr[], int l, int r) {
+        for (int i = l + 1; i <= r; i++) {
+            T e = arr[i];
+            int j;
+            for (j = i; j > l && arr[j - 1] > e; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = e;
+        }
+    }
     //冒泡排序
     /*template<typename T>
     void bubbleSort(T arr[], const int n) {
@@ -121,6 +134,83 @@ namespace Sort {
                 arr[j] = e;
             }
             h /= 3;
+        }
+    }
+
+    // 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+    template<typename T>
+    void __merge(T arr[], const int l, const int mid, const int r) {
+        T aux[r - l + 1];
+        for (int i = l; i <= r; ++i) {
+            aux[i - l] = arr[i];
+        }
+        // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {  // 如果左半部分元素已经全部处理完毕
+                arr[k] = aux[j - l];
+                ++j;
+            } else if (j > r) {  // 如果右半部分元素已经全部处理完毕
+                arr[k] = aux[i - l];
+                ++i;
+            } else if (aux[i - l] < aux[j - l]) {  // 左半部分所指元素 < 右半部分所指元素
+                arr[k] = aux[i - l];
+                ++i;
+            } else {  // 左半部分所指元素 >= 右半部分所指元素
+                arr[k] = aux[j - l];
+                ++j;
+            }
+        }
+    }
+
+    // 递归使用归并排序,对arr[l...r]的范围进行排序
+    template<typename T>
+    void __mergeSort(T arr[], const int l, const int r) {
+        /*if (l >= r) {
+            return;
+        }*/
+        // 优化2: 对于小规模数组, 使用插入排序
+        if (r - l <= 15) {
+            insertionSort(arr, l, r);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        __mergeSort(arr, l, mid);
+        __mergeSort(arr, mid + 1, r);
+        // 优化1: 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+        // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
+        if (arr[mid] > arr[mid + 1]) {
+            __merge(arr, l, mid, r);
+        }
+    }
+
+    //归并排序
+    template<typename T>
+    void mergeSort(T arr[], const int n) {
+        __mergeSort(arr, 0, n - 1);
+    }
+
+    // 使用自底向上的归并排序算法
+    // Merge Sort BU 也是一个O(nlogn)复杂度的算法，虽然只使用两重for循环
+    // 所以，Merge Sort BU也可以在1秒之内轻松处理100万数量级的数据
+    // 注意：不要轻易根据循环层数来判断算法的复杂度，Merge Sort BU就是一个反例
+    // 比较Merge Sort和Merge Sort Bottom Up两种排序算法的性能效率
+    // 整体而言, 两种算法的效率是差不多的。但是如果进行仔细测试, 自底向上的归并排序会略胜一筹。
+    // 不过Merge Sort Bottom Up 有一个非常重要的作用，这个代码中没有使用数组的索引获取元素特性，就因如此可以非常好的在nlog(n)的时间对链表这样的数据结构进行排序
+    template<typename T>
+    void mergeSortBU(T arr[], const int n) {
+        // Merge Sort Bottom Up 优化
+        // 对于小数组, 使用插入排序优化
+        for (int i = 0; i < n; i += 16) {
+            insertionSort(arr, i, std::min(i + 15, n - 1));
+        }
+        for (int sz = 16; sz < n; sz += sz) {
+            for (int i = 0; i < n - sz; i += sz + sz) {
+                // 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+                if (arr[i + sz - 1] > arr[i + sz]) {
+                    __merge(arr, i, i + sz - 1, std::min(i + sz + sz - 1, n - 1));
+                }
+            }
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -4,33 +4,39 @@
 using namespace std;
 
 int main() {
-    int n = 20000;
+    int n = 2000000;
 
     cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
-    int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
+    int *arr1 = SortTestHelper::generateRandomArray(n, 0,n);
     int *arr2 = SortTestHelper::copyIntArray(arr1, n);
     int *arr3 = SortTestHelper::copyIntArray(arr1, n);
     int *arr4 = SortTestHelper::copyIntArray(arr1, n);
-
-    /*SortTestHelper::printArray(arr1, n);
-    SortTestHelper::printArray(arr2, n);
-    SortTestHelper::printArray(arr3, n);
-    SortTestHelper::printArray(arr4, n);*/
-
-    SortTestHelper::testSort("Insertion Sort", Sort::insertionSort, arr1, n);
-    SortTestHelper::testSort("Selection Sort", Sort::selectionSort, arr2, n);
-    SortTestHelper::testSort("bubbleSort Sort", Sort::bubbleSort, arr3, n);
-    SortTestHelper::testSort("bubbleSort Sort", Sort::shellSort, arr4, n);
+    int *arr5 = SortTestHelper::copyIntArray(arr1, n);
 
 //    SortTestHelper::printArray(arr1, n);
 //    SortTestHelper::printArray(arr2, n);
 //    SortTestHelper::printArray(arr3, n);
 //    SortTestHelper::printArray(arr4, n);
+//    SortTestHelper::printArray(arr5, n);
+
+
+    //SortTestHelper::testSort("Insertion Sort", Sort::insertionSort, arr1, n);
+    //SortTestHelper::testSort("Selection Sort", Sort::selectionSort, arr2, n);
+    //SortTestHelper::testSort("bubbleSort Sort", Sort::bubbleSort, arr3, n);
+    SortTestHelper::testSort("shellSort Sort", Sort::shellSort, arr4, n);
+    SortTestHelper::testSort("mergeSort Sort", Sort::mergeSort, arr5, n);
+
+//    SortTestHelper::printArray(arr1, n);
+//    SortTestHelper::printArray(arr2, n);
+//    SortTestHelper::printArray(arr3, n);
+//    SortTestHelper::printArray(arr4, n);
+//    SortTestHelper::printArray(arr5, n);
 
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
+    delete[] arr4;
     delete[] arr4;
 
     cout << endl;
